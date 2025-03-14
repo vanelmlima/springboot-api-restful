d3ac706d9e3aa3615c506ebf93930f8889dd79e1
Vanessa Lima <vlml2001lima@gmail.com>
2025-03-14 15:29:49 -0300

    first commit

---
+++ src/main/java/vlml/springboot_api_restful/SpringbootApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package vlml.springboot_api_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vlml/springboot_api_restful/SpringbootApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package vlml.springboot_api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
